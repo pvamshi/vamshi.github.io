9759da04fa3620a4da662e7b1eb4f28dfc01b25a
Vamshi Krishna <pvamshi@gmail.com>
2017-02-04 21:19:05 +0530

    removing build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-git config --global -l #<= totally unnecessary, but "fixes" problem?
-git config --global user.email circleci@circleci
-git config --global user.name CircleCI
-harp compile
-cd www && git checkout gh-pages
-cd www && git commit -m "build" -a 
-cd www && git push origin gh-pages
