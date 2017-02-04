28a660edaaa9f9f5951e6e4c16d4e137456b4e4f
Vamshi Krishna <pvamshi@gmail.com>
2017-02-04 10:38:19 +0530

    removing www

--- www/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    var colors = ['blue','purple','indigo','light-blue','cyan','teal','pink','orange','red','green','deep-orange','brown'];
-    var color = colors[Math.floor(Math.random()*colors.length )];
-    $('.main-color').addClass(color);
-    $('.main-color-text').addClass(color+'-text');
-});
