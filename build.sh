6bdeeda5ea6be80ec22b535566fbca9548fe0215
Vamshi Krishna <pvamshi@gmail.com>
2017-02-04 18:34:28 +0530

    Updating build.sh for config user.email, name

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+git config --global -l #<= totally unnecessary, but "fixes" problem?
+git config --global user.email circleci@circleci
+git config --global user.name CircleCI
 harp compile
 cd www
 git checkout gh-pages
