b59c223ad7c14769010dd6b509602f055e40e571
Vamshi Krishna <pvamshi@gmail.com>
2017-02-04 19:06:12 +0530

    Fixing build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,6 @@ git config --global -l #<= totally unnecessary, but "fixes" problem?
 git config --global user.email circleci@circleci
 git config --global user.name CircleCI
 harp compile
-cd www
-git checkout gh-pages
-git commit -m "build" -a 
-git push origin gh-pages
-cd ..
+cd www && git checkout gh-pages
+cd www && git commit -m "build" -a 
+cd www && git push origin gh-pages
