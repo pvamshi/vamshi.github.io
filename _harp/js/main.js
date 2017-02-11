c35d04413b4dc5c8b5c54f2c69c2150a1defb4d3
Vamshi krishna <pvamshi@gmail.com>
2017-02-11 10:41:38 +0530

    Prevent color change in every refresh

--- _harp/js/main.js
+++ _harp/js/main.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
     var colors = ['blue','purple','indigo','light-blue','cyan','teal','pink','orange','red','green','deep-orange','brown'];
-    var color = colors[Math.floor(Math.random()*colors.length )];
+    var color = (sessionStorage && sessionStorage.getItem('theme') ) || colors[Math.floor(Math.random()*colors.length )];
+    sessionStorage && sessionStorage.setItem('theme', color );
     $('.main-color').addClass(color);
     $('.main-color-text').addClass(color+'-text');
     $('table').addClass('striped');
     $('img').addClass('responsive-img');
 });
 
+function changeTheme(){
+    sessionStorage && sessionStorage.removeItem('theme');
+    return true;
+}
